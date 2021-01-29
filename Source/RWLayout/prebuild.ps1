4907238897be55346ba97b9ee26a8abf7a80e533
- <krypt-lynx@yandex.ru>
2021-01-29 20:57:49 +0200

    Update prebuild.ps1

--- Source/RWLayout/prebuild.ps1
+++ Source/RWLayout/prebuild.ps1
@@ -1,7 +1,15 @@
-param([string]$ProjectDir, [string]$TargetDir);
-Echo "parameters:"
-Echo "ProjectDir: $ProjectDir"
-Echo "TargetDir: $TargetDir"
+$ProjectDir = $Env:ProjectDir
+$TargetDir = $Env:TargetDir
+
+Echo "Project directory: $ProjectDir"
+Echo "Target directory: $TargetDir"
+
+if (($ProjectDir -eq $null) -or
+	($TargetDir -eq $null))
+{
+	Echo "Paths not set; exiting"
+	exit
+}
 
 $version = git describe --tag  --dirty --always 
 $version | Out-File -NoNewline "$ProjectDir\git.txt"
