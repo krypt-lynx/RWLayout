1261f437357ac11597a1dcfa9ab48aa654100e86
- <krypt-lynx@yandex.ru>
2021-02-03 17:52:54 +0200

    Bindable fix

--- Source/RWLayout/Serialization/Bindable.cs
+++ Source/RWLayout/Serialization/Bindable.cs
@@ -8,19 +8,38 @@ using Verse;
 
 namespace RWLayout.alpha2
 {
+    /// <summary>
+    /// Binding mode
+    /// </summary>
     public enum BindingMode
     {
-        Never,
+        /// <summary>
+        /// Copy value on command
+        /// </summary>
         Manual,
+        /// <summary>
+        /// Copy value on access
+        /// </summary>
         Auto
     }
 
+    /// <summary>
+    /// Class binding values in runtime
+    /// </summary>
+    /// <typeparam name="TProperty"></typeparam>
     public class Bindable<TProperty>
     {
         WeakReference binded;
         MemberHandler bindedMember;
 
+        /// <summary>
+        /// Read mode
+        /// </summary>
         public BindingMode ReadMode = BindingMode.Auto;
+
+        /// <summary>
+        /// Write mode
+        /// </summary>
         public BindingMode WriteMode = BindingMode.Auto;
 
         private TProperty value;
@@ -47,8 +66,16 @@ namespace RWLayout.alpha2
             }
         }
 
+        /// <summary>
+        /// Creates Bindable
+        /// </summary>
         public Bindable() { }
 
+        /// <summary>
+        /// Creates bindable
+        /// </summary>
+        /// <param name="readMode">Binding mode for reads</param>
+        /// <param name="writeMode">Binding mode for writes</param>
         public Bindable(BindingMode readMode, BindingMode writeMode)
         {
             ReadMode = readMode;
@@ -63,19 +90,25 @@ namespace RWLayout.alpha2
             SynchronizeFrom();
         }
 
+        /// <summary>
+        /// Copy value from binded property
+        /// </summary>
         public void SynchronizeFrom()
         {
             if (binded?.Target != null)
             {
-                Value = (TProperty)bindedMember.GetValue(binded.Target);
+                value = (TProperty)bindedMember.GetValue(binded.Target);
             }
         }
 
+        /// <summary>
+        /// Copy value to binded property
+        /// </summary>
         public void SynchronizeTo()
         {
             if (binded?.Target != null)
             {
-                bindedMember.SetValue(binded.Target, Value);
+                bindedMember.SetValue(binded.Target, value);
             }
         }
     }
