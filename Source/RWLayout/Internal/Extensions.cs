ec94dcbceac8ae1ada604c020910f54ba3bf4b1d
- <krypt-lynx@yandex.ru>
2021-02-11 02:00:42 +0200

    Update Extensions.cs

--- Source/RWLayout/Internal/Extensions.cs
+++ Source/RWLayout/Internal/Extensions.cs
@@ -8,24 +8,8 @@ using System.Xml;
 
 namespace RWLayout.alpha2
 {
-    public static class XmlNodeListExtentions
+    internal static class XmlNodeListExtentions
     {
-        /// <summary>
-        /// Converts IEnumerable to IEnumerable&lt;T&gt;
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="list"></param>
-        /// <returns></returns>
-        [Obsolete("User .Cast<T>() instead")]
-        public static IEnumerable<T> AsEnumerable<T>(this IEnumerable list)
-        {
-            foreach (var node in list)
-            {
-                yield return (T)node;
-            }
-        }
-
-
         static Dictionary<Type, XmlNodeType> nodeTypesMap = new Dictionary<Type, XmlNodeType>
         {
             //{ typeof(?), XmlNodeType.None},
@@ -55,7 +39,7 @@ namespace RWLayout.alpha2
             if (!nodeTypesMap.TryGetValue(type, out nodeType))
             {
                 yield break;
-            } 
+            }
             else
             {
                 foreach (XmlNode element in list)
@@ -66,11 +50,11 @@ namespace RWLayout.alpha2
                     }
                 }
             }
-        } 
+        }
 
         public static IEnumerable<T> WhereTypeIs<T>(this IEnumerable list)
         {
-            
+
             Type type = typeof(T);
             foreach (var element in list)
             {
@@ -79,6 +63,25 @@ namespace RWLayout.alpha2
                     yield return (T)element;
                 }
             }
+        }
+    }
+
+    /// <summary>
+    /// Internal class with linq extention.
+    /// Those methods are not that complex to make it public, but because they are internal I can modify freely them.
+    /// </summary>
+    internal static class RWLinq
+    { 
+        /// <summary>
+        /// Wraps this object instance into an IEnumerable&lt;T&gt;
+        /// consisting of a single item.
+        /// </summary>
+        /// <typeparam name="T"> Type of the object. </typeparam>
+        /// <param name="item"> The instance that will be wrapped. </param>
+        /// <returns> An IEnumerable&lt;T&gt; consisting of a single item. </returns>
+        public static IEnumerable<T> Yield<T>(this T item)
+        {
+            yield return item;
         }
     }
 }
