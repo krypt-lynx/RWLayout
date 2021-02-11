81def69d21f9a9a2f884baad16dcaeb4c38a5a1c
- <krypt-lynx@yandex.ru>
2021-02-12 01:43:57 +0200

    Generic type constraints

--- Source/RWLayout/FastAccess/Dynamic+InstanceRetMethod.cs
+++ Source/RWLayout/FastAccess/Dynamic+InstanceRetMethod.cs
@@ -9,37 +9,37 @@ namespace RWLayout.alpha2.FastAccess
 {
     public partial class Dynamic
     {
-        public static Func<TInstance, TResult> InstanceRetMethod<TInstance, TResult>(string methodName, BindingFlags bindingAttr = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+        public static Func<TInstance, TResult> InstanceRetMethod<TInstance, TResult>(string methodName, BindingFlags bindingAttr = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic) where TInstance : class
         {
             var method = typeof(TInstance).GetMethod(methodName, bindingAttr);
             return InstanceRetMethod<TInstance, TResult>(method);
         }
 
-        public static Func<TInstance, TArg0, TResult> InstanceRetMethod<TInstance, TArg0, TResult>(string methodName, BindingFlags bindingAttr = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+        public static Func<TInstance, TArg0, TResult> InstanceRetMethod<TInstance, TArg0, TResult>(string methodName, BindingFlags bindingAttr = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic) where TInstance : class
         {
             var method = typeof(TInstance).GetMethod(methodName, bindingAttr);
             return InstanceRetMethod<TInstance, TArg0, TResult>(method);
         }
 
-        public static Func<TInstance, TArg0, TArg1, TResult> InstanceRetMethod<TInstance, TArg0, TArg1, TResult>(string methodName, BindingFlags bindingAttr = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+        public static Func<TInstance, TArg0, TArg1, TResult> InstanceRetMethod<TInstance, TArg0, TArg1, TResult>(string methodName, BindingFlags bindingAttr = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic) where TInstance : class
         {
             var method = typeof(TInstance).GetMethod(methodName, bindingAttr);
             return InstanceRetMethod<TInstance, TArg0, TArg1, TResult>(method);
         }
 
-        public static Func<TInstance, TArg0, TArg1, TArg2, TResult> InstanceRetMethod<TInstance, TArg0, TArg1, TArg2, TResult>(string methodName, BindingFlags bindingAttr = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+        public static Func<TInstance, TArg0, TArg1, TArg2, TResult> InstanceRetMethod<TInstance, TArg0, TArg1, TArg2, TResult>(string methodName, BindingFlags bindingAttr = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic) where TInstance : class
         {
             var method = typeof(TInstance).GetMethod(methodName, bindingAttr);
             return InstanceRetMethod<TInstance, TArg0, TArg1, TArg2, TResult>(method);
         }
 
-        public static Func<TInstance, TArg0, TArg1, TArg2, TArg3, TResult> InstanceRetMethod<TInstance, TArg0, TArg1, TArg2, TArg3, TResult>(string methodName, BindingFlags bindingAttr = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+        public static Func<TInstance, TArg0, TArg1, TArg2, TArg3, TResult> InstanceRetMethod<TInstance, TArg0, TArg1, TArg2, TArg3, TResult>(string methodName, BindingFlags bindingAttr = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic) where TInstance : class
         {
             var method = typeof(TInstance).GetMethod(methodName, bindingAttr);
             return InstanceRetMethod<TInstance, TArg0, TArg1, TArg2, TArg3, TResult>(method);
         }
 
-        public static Func<TInstance, TArg0, TArg1, TArg2, TArg3, TArg4, TResult> InstanceRetMethod<TInstance, TArg0, TArg1, TArg2, TArg3, TArg4, TResult>(string methodName, BindingFlags bindingAttr = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+        public static Func<TInstance, TArg0, TArg1, TArg2, TArg3, TArg4, TResult> InstanceRetMethod<TInstance, TArg0, TArg1, TArg2, TArg3, TArg4, TResult>(string methodName, BindingFlags bindingAttr = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic) where TInstance : class
         {
             var method = typeof(TInstance).GetMethod(methodName, bindingAttr);
             return InstanceRetMethod<TInstance, TArg0, TArg1, TArg2, TArg3, TArg4, TResult>(method);
@@ -48,13 +48,13 @@ namespace RWLayout.alpha2.FastAccess
 
 
 
-        public static Func<TInstance, TResult> InstanceRetMethod<TInstance, TResult>(MethodInfo method)
+        public static Func<TInstance, TResult> InstanceRetMethod<TInstance, TResult>(MethodInfo method) where TInstance : class
         {
             return (Func<TInstance, TResult>)CreateMethodCaller(typeof(Func<TInstance, TResult>), 
                 method, method.ReturnType, method.DeclaringType);
         }
 
-        public static Func<TInstance, TArg0, TResult> InstanceRetMethod<TInstance, TArg0, TResult>(MethodInfo method)
+        public static Func<TInstance, TArg0, TResult> InstanceRetMethod<TInstance, TArg0, TResult>(MethodInfo method) where TInstance : class
         {
 
             return (Func<TInstance, TArg0, TResult>)CreateMethodCaller(typeof(Func<TInstance, TArg0, TResult>), 
@@ -62,28 +62,28 @@ namespace RWLayout.alpha2.FastAccess
                 method.GetParameters().Select(x => x.ParameterType).ToArray());
         }
 
-        public static Func<TInstance, TArg0, TArg1, TResult> InstanceRetMethod<TInstance, TArg0, TArg1, TResult>(MethodInfo method)
+        public static Func<TInstance, TArg0, TArg1, TResult> InstanceRetMethod<TInstance, TArg0, TArg1, TResult>(MethodInfo method) where TInstance : class
         {
             return (Func<TInstance, TArg0, TArg1, TResult>)CreateMethodCaller(typeof(Func<TInstance, TArg0, TArg1, TResult>), 
                 method, method.ReturnType, method.DeclaringType,
                 method.GetParameters().Select(x => x.ParameterType).ToArray());
         }
 
-        public static Func<TInstance, TArg0, TArg1, TArg2, TResult> InstanceRetMethod<TInstance, TArg0, TArg1, TArg2, TResult>(MethodInfo method)
+        public static Func<TInstance, TArg0, TArg1, TArg2, TResult> InstanceRetMethod<TInstance, TArg0, TArg1, TArg2, TResult>(MethodInfo method) where TInstance : class
         {
             return (Func<TInstance, TArg0, TArg1, TArg2, TResult>)CreateMethodCaller(typeof(Func<TInstance, TArg0, TArg1, TArg2, TResult>),
                 method, method.ReturnType, method.DeclaringType,
                 method.GetParameters().Select(x => x.ParameterType).ToArray());
         }
 
-        public static Func<TInstance, TArg0, TArg1, TArg2, TArg3, TResult> InstanceRetMethod<TInstance, TArg0, TArg1, TArg2, TArg3, TResult>(MethodInfo method)
+        public static Func<TInstance, TArg0, TArg1, TArg2, TArg3, TResult> InstanceRetMethod<TInstance, TArg0, TArg1, TArg2, TArg3, TResult>(MethodInfo method) where TInstance : class
         {
             return (Func<TInstance, TArg0, TArg1, TArg2, TArg3, TResult>)CreateMethodCaller(typeof(Func<TInstance, TArg0, TArg1, TArg2, TArg3, TResult>),
                 method, method.ReturnType, method.DeclaringType,
                 method.GetParameters().Select(x => x.ParameterType).ToArray());
         }
 
-        public static Func<TInstance, TArg0, TArg1, TArg2, TArg3, TArg4, TResult> InstanceRetMethod<TInstance, TArg0, TArg1, TArg2, TArg3, TArg4, TResult>(MethodInfo method)
+        public static Func<TInstance, TArg0, TArg1, TArg2, TArg3, TArg4, TResult> InstanceRetMethod<TInstance, TArg0, TArg1, TArg2, TArg3, TArg4, TResult>(MethodInfo method) where TInstance : class
         {
             return (Func<TInstance, TArg0, TArg1, TArg2, TArg3, TArg4, TResult>)CreateMethodCaller(typeof(Func<TInstance, TArg0, TArg1, TArg2, TArg3, TArg4, TResult>),
                 method, method.ReturnType, method.DeclaringType,
--- Source/RWLayout/FastAccess/Dynamic+InstanceVoidMethod.cs
+++ Source/RWLayout/FastAccess/Dynamic+InstanceVoidMethod.cs
@@ -9,37 +9,37 @@ namespace RWLayout.alpha2.FastAccess
 {
     public partial class Dynamic
     {
-        public static Action<TInstance> InstanceVoidMethod<TInstance>(string methodName, BindingFlags bindingAttr = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+        public static Action<TInstance> InstanceVoidMethod<TInstance>(string methodName, BindingFlags bindingAttr = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic) where TInstance : class
         {
             var method = typeof(TInstance).GetMethod(methodName, bindingAttr);
             return InstanceVoidMethod<TInstance>(method);
         }
 
-        public static Action<TInstance, TArg0> InstanceVoidMethod<TInstance, TArg0>(string methodName, BindingFlags bindingAttr = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+        public static Action<TInstance, TArg0> InstanceVoidMethod<TInstance, TArg0>(string methodName, BindingFlags bindingAttr = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic) where TInstance : class
         {
             var method = typeof(TInstance).GetMethod(methodName, bindingAttr);
             return InstanceVoidMethod<TInstance, TArg0>(method);
         }
 
-        public static Action<TInstance, TArg0, TArg1> InstanceVoidMethod<TInstance, TArg0, TArg1>(string methodName, BindingFlags bindingAttr = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+        public static Action<TInstance, TArg0, TArg1> InstanceVoidMethod<TInstance, TArg0, TArg1>(string methodName, BindingFlags bindingAttr = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic) where TInstance : class
         {
             var method = typeof(TInstance).GetMethod(methodName, bindingAttr);
             return InstanceVoidMethod<TInstance, TArg0, TArg1>(method);
         }
 
-        public static Action<TInstance, TArg0, TArg1, TArg2> InstanceVoidMethod<TInstance, TArg0, TArg1, TArg2>(string methodName, BindingFlags bindingAttr = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+        public static Action<TInstance, TArg0, TArg1, TArg2> InstanceVoidMethod<TInstance, TArg0, TArg1, TArg2>(string methodName, BindingFlags bindingAttr = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic) where TInstance : class
         {
             var method = typeof(TInstance).GetMethod(methodName, bindingAttr);
             return InstanceVoidMethod<TInstance, TArg0, TArg1, TArg2>(method);
         }
 
-        public static Action<TInstance, TArg0, TArg1, TArg2, TArg3> InstanceVoidMethod<TInstance, TArg0, TArg1, TArg2, TArg3>(string methodName, BindingFlags bindingAttr = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+        public static Action<TInstance, TArg0, TArg1, TArg2, TArg3> InstanceVoidMethod<TInstance, TArg0, TArg1, TArg2, TArg3>(string methodName, BindingFlags bindingAttr = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic) where TInstance : class
         {
             var method = typeof(TInstance).GetMethod(methodName, bindingAttr);
             return InstanceVoidMethod<TInstance, TArg0, TArg1, TArg2, TArg3>(method);
         }
 
-        public static Action<TInstance, TArg0, TArg1, TArg2, TArg3, TArg4> InstanceVoidMethod<TInstance, TArg0, TArg1, TArg2, TArg3, TArg4>(string methodName, BindingFlags bindingAttr = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+        public static Action<TInstance, TArg0, TArg1, TArg2, TArg3, TArg4> InstanceVoidMethod<TInstance, TArg0, TArg1, TArg2, TArg3, TArg4>(string methodName, BindingFlags bindingAttr = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic) where TInstance : class
         {
             var method = typeof(TInstance).GetMethod(methodName, bindingAttr);
             return InstanceVoidMethod<TInstance, TArg0, TArg1, TArg2, TArg3, TArg4>(method);
@@ -48,40 +48,40 @@ namespace RWLayout.alpha2.FastAccess
 
 
 
-        public static Action<TInstance> InstanceVoidMethod<TInstance>(MethodInfo method)
+        public static Action<TInstance> InstanceVoidMethod<TInstance>(MethodInfo method) where TInstance : class
         {
             return (Action<TInstance>)CreateMethodCaller(typeof(Action<TInstance>), method, null, method.DeclaringType);
         }
 
-        public static Action<TInstance, TArg0> InstanceVoidMethod<TInstance, TArg0>(MethodInfo method)
+        public static Action<TInstance, TArg0> InstanceVoidMethod<TInstance, TArg0>(MethodInfo method) where TInstance : class
         {
             return (Action<TInstance, TArg0>)CreateMethodCaller(typeof(Action<TInstance, TArg0>), method, null, method.DeclaringType, 
                 method.GetParameters().Select(x => x.ParameterType).ToArray());
 
         }
 
-        public static Action<TInstance, TArg0, TArg1> InstanceVoidMethod<TInstance, TArg0, TArg1>(MethodInfo method)
+        public static Action<TInstance, TArg0, TArg1> InstanceVoidMethod<TInstance, TArg0, TArg1>(MethodInfo method) where TInstance : class
         {
             return (Action<TInstance, TArg0, TArg1>)CreateMethodCaller(typeof(Action<TInstance, TArg0, TArg1>), method, null, method.DeclaringType,
                 method.GetParameters().Select(x => x.ParameterType).ToArray());
 
         }
 
-        public static Action<TInstance, TArg0, TArg1, TArg2> InstanceVoidMethod<TInstance, TArg0, TArg1, TArg2>(MethodInfo method)
+        public static Action<TInstance, TArg0, TArg1, TArg2> InstanceVoidMethod<TInstance, TArg0, TArg1, TArg2>(MethodInfo method) where TInstance : class
         {
             return (Action<TInstance, TArg0, TArg1, TArg2>)CreateMethodCaller(typeof(Action<TInstance, TArg0, TArg1, TArg2>), method, null, method.DeclaringType,
                 method.GetParameters().Select(x => x.ParameterType).ToArray());
 
         }
 
-        public static Action<TInstance, TArg0, TArg1, TArg2, TArg3> InstanceVoidMethod<TInstance, TArg0, TArg1, TArg2, TArg3>(MethodInfo method)
+        public static Action<TInstance, TArg0, TArg1, TArg2, TArg3> InstanceVoidMethod<TInstance, TArg0, TArg1, TArg2, TArg3>(MethodInfo method) where TInstance : class
         {
             return (Action<TInstance, TArg0, TArg1, TArg2, TArg3>)CreateMethodCaller(typeof(Action<TInstance, TArg0, TArg1, TArg2, TArg3>), method, null, method.DeclaringType,
                 method.GetParameters().Select(x => x.ParameterType).ToArray());
 
         }
 
-        public static Action<TInstance, TArg0, TArg1, TArg2, TArg3, TArg4> InstanceVoidMethod<TInstance, TArg0, TArg1, TArg2, TArg3, TArg4>(MethodInfo method)
+        public static Action<TInstance, TArg0, TArg1, TArg2, TArg3, TArg4> InstanceVoidMethod<TInstance, TArg0, TArg1, TArg2, TArg3, TArg4>(MethodInfo method) where TInstance : class
         {
             return (Action<TInstance, TArg0, TArg1, TArg2, TArg3, TArg4>)CreateMethodCaller(typeof(Action<TInstance, TArg0, TArg1, TArg2, TArg3, TArg4>), method, null, method.DeclaringType,
                 method.GetParameters().Select(x => x.ParameterType).ToArray());
